0b2a4bcce85f14d7e436f8c91b6f8db52e65535c
Maxime Feingeischt <mfeingesicht@gmail.com>
2019-07-12 15:51:27 +0200

    Added code for GPS and PmodNAV IMU

---
+++ sensors/IMU_pmodnav.py
@@ -0,0 +1,67 @@
+import binascii
+import Adafruit_GPIO.I2C as I2C
+import struct
+import time
+
+BUS = 1               # I2C bus to use on the BeagleBone Blue
+I2C_ADDR = 0X6B          # I2C address of the IMU
+
+ADDR_WHO_AM_I  = 0X0F # I2C register address for the WHO_AM_I of the IMU
+IDENTITY = 0X68       # Expected value of the WHO_AM_I of the IMU
+
+ACCEL_CONFIG = 0X1F   # I2C register address for the accelerometer configuration
+GYRO_CONFIG = 0X10    # I2C register address for the gyroscope configuration
+
+ADDR_TEMP_OUT_L =   0X15 # I2C register address for the lower 8 bits of the temperature measurement
+ADDR_TEMP_OUT_H =   0X16 # I2C register address for the higher 8 bits of the temperature measurement
+
+ADDR_ACCEL_XOUT_L = 0X28 # I2C register address for the lower 8 bits of the x-axis acceleration measurement
+ADDR_ACCEL_XOUT_H = 0X29 # I2C register address for the higher 8 bits of the x-axis acceleration measurement
+ADDR_ACCEL_YOUT_L = 0X2A # I2C register address for the lower 8 bits of the y-axis acceleration measurement
+ADDR_ACCEL_YOUT_H = 0X2B # I2C register address for the higher 8 bits of the y-axis acceleration measurement
+ADDR_ACCEL_ZOUT_L = 0X2C # I2C register address for the lower 8 bits of the z-axis acceleration measurement
+ADDR_ACCEL_ZOUT_H = 0X2D # I2C register address for the higher 8 bits of the z-axis acceleration measurement
+
+ADDR_GYRO_XOUT_L =  0X18 # I2C register address for the lower 8 bits of the x-axis angular velocity measurement
+ADDR_GYRO_XOUT_H =  0X19 # I2C register address for the higher 8 bits of the x-axis angular velocity measurement
+ADDR_GYRO_YOUT_L =  0X1A # I2C register address for the lower 8 bits of the y-axis angular velocity measurement
+ADDR_GYRO_YOUT_H =  0X1B # I2C register address for the higher 8 bits of the y-axis angular velocity measurement
+ADDR_GYRO_ZOUT_L =  0X1C # I2C register address for the lower 8 bits of the z-axis angular velocity measurement
+ADDR_GYRO_ZOUT_H =  0X1D # I2C register address for the higher 8 bits of the z-axis angular velocity measurement
+
+
+class IMU(object):
+
+    imu = None
+
+    def __init__(self):
+        self.i2c = I2C.Device(I2C_ADDR, BUS)
+
+        # Check that WHO_AM_I matches the expected value
+        who_am_i = self.i2c.readList(ADDR_WHO_AM_I, 1) # read the WHO_AM_I register
+        if hex(who_am_i[0]) != str(hex(IDENTITY)):
+            raise ValueError('IMU NOT IDENTIFIED! the read WHO_AM_I (' + str(who_am_i) + ') does not match the expected one (' + str(IDENTITY) + ')')
+
+        self.i2c.write8(ACCEL_CONFIG, 0b00111000) # Set accelerometer sensitivity to +/- 2g
+        self.i2c.write8(GYRO_CONFIG, 0b11100000)  # Set gyroscope sensitivity to +/- 245 dps
+
+        self.temp_sensitivity = 16.0            # from LSM9DS1's datasheet
+        self.acc_sensitivity = 4 * 1000 / 65536 # 4000mg (+/- 2g) range on 2 bytes (2^16 = 65536)
+        self.gyro_sensitivity = 0.00875 / 1000  # from LSM9DS1's datasheet
+
+    def get_imu_data(self):
+        # Get data from the I2C transmission
+        imu_data = (struct.unpack('<hhhhhhh', (self.i2c.readList(ADDR_TEMP_OUT_L, 14))))
+        
+        # Process IMU data
+        temp = 25 + imu_data[0] / self.temp_sensitivity
+
+        gx = imu_data[1] * self.gyro_sensitivity
+        gy = imu_data[2] * self.gyro_sensitivity
+        gz = imu_data[3] * self.gyro_sensitivity
+
+        accx = imu_data[4] * self.acc_sensitivity
+        accy = imu_data[5] * self.acc_sensitivity
+        accz = imu_data[6] * self.acc_sensitivity
+
+        return [temp, accx, accy, accz, gx, gy, gz]
---
+++ sensors/gps.py
@@ -0,0 +1,183 @@
+import Adafruit_BBIO.UART as UART
+import serial
+import math
+import time
+
+# Select the port
+port = "/dev/ttyO5"
+
+# Select the sampling time
+samplingTime = 0.2  # secs
+
+# Define the PMTK configurations strings to send to the GPS according to the NMEA PMTK protocol
+# PMTK Packet User Manual       : https://cdn.sparkfun.com/assets/parts/1/2/2/8/0/PMTK_Packet_User_Manual.pdf
+# NMEA sentences                : http://aprs.gids.nl/nmea/
+# PMTK/NMEA checksum calculator : http://www.hhhh.org/wiml/proj/nmeaxor.html
+
+# Data update rates
+PMTK_SET_NMEA_UPDATE_1HZ  = "$PMTK220,1000*1F" # 1 Hz
+PMTK_SET_NMEA_UPDATE_2HZ  = "$PMTK220,500*2B"  # 2 Hz
+PMTK_SET_NMEA_UPDATE_5HZ  = "$PMTK220,200*2C"  # 5 Hz
+PMTK_SET_NMEA_UPDATE_10HZ = "$PMTK220,100*2F"  # 10 Hz
+
+# Baud rate
+PMTK_SET_BAUD_115200 = "$PMTK251,115200*1F" # 115200 bps
+PMTK_SET_BAUD_57600  = "$PMTK251,57600*2C"  # 57600 bps
+PMTK_SET_BAUD_9600   = "$PMTK251,9600*17"   # 9600 bps
+
+# NMEA sentences to output
+PMTK_SET_NMEA_OUTPUT_RMCGGA = "$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28"  # turn on GPRMC and GPGGA
+PMTK_SET_NMEA_OUTPUT_RMCONLY = "$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29" # turn on only the GPRMC sentence
+PMTK_SET_NMEA_OUTPUT_ALLDATA = "$PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0*28" # turn on ALL THE DATA
+PMTK_SET_NMEA_OUTPUT_OFF     = "$PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28" # turn off output
+PMTK_SET_NMEA_OUTPUT_GLLONLY = "$PMTK314,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29" # turn on only the GPGLL sentence
+PMTK_SET_NMEA_OUTPUT_VTGONLY = "$PMTK314,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29" # turn on only the GPVTG
+PMTK_SET_NMEA_OUTPUT_GGAONLY = "$PMTK314,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29" # turn on just the GPGGA
+PMTK_SET_NMEA_OUTPUT_GSAONLY = "$PMTK314,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29" # turn on just the GPGSA
+PMTK_SET_NMEA_OUTPUT_GSVONLY = "$PMTK314,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0*29" # turn on just the GPGSV
+
+# GPS status
+PMTK_STANDBY         = "$PMTK161,0*28"     # standby command & boot successful message
+PMTK_AWAKE           = "$PMTK010,002*2D"   # Wake up
+
+# Antenna status
+PGCMD_ANTENNA   = "$PGCMD,33,1*6C" # request for updates on antenna status
+PGCMD_NOANTENNA = "$PGCMD,33,0*6D" # don't show antenna status messages
+
+
+class GPS(object):
+
+    gps = None
+
+    def __init__(self):
+        # Open the serial connection at the default baudrate of 9600
+        self.ser1 = serial.Serial(port, 9600, timeout=1000)
+
+        # Change the baud rate to 115200 bps
+        self.ser1.write(PMTK_SET_BAUD_115200 + "\r\n")
+
+        # Restart the serial connection
+        self.ser1.flush()
+        self.ser1.close()
+        self.ser1 = serial.Serial(port, 115200, timeout=1000)
+
+        # Choose the type of the GPS data output
+        # self.ser1.write(PMTK_SET_NMEA_OUTPUT_RMCGGA+ "\r\n")   # turn on GPRMC and GPGGA
+        self.ser1.write(PMTK_SET_NMEA_OUTPUT_RMCONLY + "\r\n") # turn on only the GPRMC sentence
+
+        # Choose the frequency of the GPS data output
+        self.ser1.write(PMTK_SET_NMEA_UPDATE_5HZ + "\r\n") # select a 5Hz output rate
+
+        self.lat_ini, self.lon_ini = self.get_latlon()
+
+
+    def get_position(self):
+        lat, lon = self.get_latlon()
+        dx = (lon - self.lon_ini) * 40000 * math.cos((lat + self.lat_ini) * math.pi / 360) / 360
+        dy = (lat - self.lat_ini) * 40000 / 360
+        return dx, dy
+
+
+    def get_latlon(self):
+        readall = self.ser1.readline().split('\r\n')  # Read data from the GPS
+        # print 'Communication costs ' + str(ReadFintime-starttime) # print communication time
+
+        # Process data
+        for i in range(0, len(readall) - 1):
+            line = readall[i]      # Extract one NMEA sentence from the received data
+            line = line.split("*") # Remove "*", the final character of a NMEA sentence
+            checksum = line[1]     # Get the checksum
+
+            line = line[0].split(",", 19) # Split comma-separated values
+            self.process_data(line) # Process data
+        return self.latitude, self.longitude
+
+
+    def process_data(self,line):
+        if line[0] == '$GPGGA':
+            gga = line
+            self.utc = gga[1]
+            self.latitude = gga[2]
+            self.ns_Indicator = gga[3]
+            self.longitude = gga[4]
+            self.ew_Indicator = gga[5]
+            self.position_Indicator = gga[6]
+            self.satelite_Used = gga[7]
+            self.hdop = gga[8]
+            self.msl_altitude = gga[9]
+            self.altitude_units = gga[10]
+            self.geoidal_Separation = gga[11]
+            self.separation_unit = gga[12]
+            self.age_of_Diff_Corr = gga[13]  # null when DGPS not used
+        elif line[0] == '$GPGSA':
+            gsa = line
+            self.mode1 = gsa[1]
+            self.mode2 = gsa[2]
+            self.satelite_used_channel1 = gsa[3]
+            self.satelite_used_channel2 = gsa[4]
+            self.satelite_used_channel3 = gsa[5]
+            self.satelite_used_channel4 = gsa[6]
+            self.satelite_used_channel5 = gsa[7]
+            self.satelite_used_channel6 = gsa[8]
+            self.satelite_used_channel7 = gsa[9]
+            self.satelite_used_channel8 = gsa[10]
+            self.satelite_used_channel9 = gsa[11]
+            self.satelite_used_channel10 = gsa[12]
+            self.satelite_used_channel11 = gsa[13]
+            self.satelite_used_channel12 = gsa[14]
+            self.pdop = gsa[15]
+            self.hdop = gsa[16]
+            self.vdop = gsa[17]
+        elif line[0] == '$GPGSV':
+            gsv = line
+            self.number_of_message = gsv[1]
+            self.msg1 = gsv[2]
+            self.satelite_in_view = gsv[3]
+            if len(gsv) > 7:
+                self.satelite_id1 = gsv[4]
+                self.elevation1 = gsv[5]
+                self.azimuth1 = gsv[6]
+                self.SNR1 = gsv[7]
+            if len(gsv) > 11:
+                self.satelite_id2 = gsv[8]
+                self.elevation2 = gsv[9]
+                self.azimuth2 = gsv[10]
+                self.SNR2 = gsv[11]
+            if len(gsv) > 15:
+                self.satelite_id3 = gsv[12]
+                self.elevation3 = gsv[13]
+                self.azimuth3 = gsv[14]
+                self.SNR3 = gsv[15]
+            if len(gsv) > 19:
+                self.satelite_id4 = gsv[16]
+                self.elevation4 = gsv[17]
+                self.azimuth4 = gsv[18]
+                self.SNR4 = gsv[19]
+        elif line[0] == '$GPRMC':
+            rmc = line
+            print rmc
+            self.UTC = rmc[1]
+            self.status = rmc[2]
+            self.Latitude = rmc[3]
+            self.NS_indicator = rmc[4]
+            self.Longitude = rmc[5]
+            self.EW_indicator = rmc[6]
+            self.Speed_over_Ground = rmc[7]
+            self.Course_over_Ground = rmc[8]
+            self.Date = rmc[9]
+            self.Magnetic_Variation = rmc[10]
+            self.Mode_rmc = rmc[11]
+        elif line[0] == '$GPVTG':
+            vtg = line
+            self.Course = vtg[1]
+            self.Reference = vtg[2]
+            self.Course2 = vtg[3]
+            self.Reference2 = vtg[4]
+            self.Speed1 = vtg[5]
+            self.Units1 = vtg[6]
+            self.Speed2 = vtg[7]
+            self.Units2 = vtg[8]
+            self.Mode = vtg[9]
+        else:
+            print "Exception happens"
+            print line
