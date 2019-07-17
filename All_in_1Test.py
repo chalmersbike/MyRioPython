799e849d628293e4a97eace9e68bb94fb6e24c14
Maxime Feingesicht <mfeingesicht@gmail.com>
2019-07-17 13:11:43 +0200

    All_In_1Test cleaned up and simplified + cont'd other cleanup

--- All_in_1Test.py
+++ All_in_1Test.py
@@ -6,100 +6,79 @@ from actuators import RearMotorDrive, SteeringMotor
 from controller import Controller
 from time import sleep
 import time
+
 # INITIAL_SPEED = 3.9
+
 class Test(object):
     def __init__(self):
+        # Safety stop
         self.safety_stop = SafetyStop()
         input1 = raw_input('Press the EStop and Enter, test the emergency stop is detectable!')
         ESign = self.safety_stop.button_check()
         while not ESign:
-
             print 'The EStop was not pressed!'
             time.sleep(0.5)
             ESign = self.safety_stop.button_check()
         if ESign:
             print 'Estop detected, please reset it if you want to test the motors'
 
-        input1 = raw_input('Press ENTER or #sample to start hallsensor test,rotate the rear wheel for the reading!')
-
+        # Hall sensor
+        number_samples_HallSensor = raw_input('Input the number of samples of press ENTER for 20 samples for the Hall sensor test, rotate the rear wheel for the reading! ')
         self.hall_sensor = HallSensor()
         start_time = time.time()
-        if not input1:
-            for x in range(1,20):
-                print 'Time=%f\t Vel = %f\n' % (time.time()-start_time,self.hall_sensor.get_velocity())
-                time.sleep(0.5)
-        else:
-            for x in range(1,int(input1)):
+        if not number_samples_HallSensor:
+            number_samples_HallSensor = 20
 
-                print 'Time=%f\t Vel = %f\n' % (time.time()-start_time, self.hall_sensor.get_velocity())
-                time.sleep(0.5)
+        for x in range(1,int(number_samples_HallSensor):
+            print 'Time=%f\t Vel = %f\n' % (time.time()-start_time,self.hall_sensor.get_velocity())
+            time.sleep(0.5)
 
-
-        input1 = raw_input('Press ENTER to start Encoder test,rotate the handle bar for the reading!')
+        # Encoder
+        number_samples_Encoder = raw_input('Input the number of samples of press ENTER for 20 samples for the encoder test, rotate the handle bar for the reading! ')
         self.encoder = Encoder()
         start_time = time.time()
         print 'ENCODER FREQNECY = %f \n' % (self.encoder.encoder.frequency)
-        if not input1:
-            for x in range(1,20):
-                print 'Time=%f\t delta = %f\n' % (time.time() - start_time, 57.29577*self.encoder.get_angle())
+        if not number_samples_Encoder:
+            number_samples_Encoder = 20
+        for x in range(1,int(number_samples_Encoder)):
+            print 'Time=%f\t delta = %f\n' % (time.time() - start_time, 57.29577 * self.encoder.get_angle())
+            time.sleep(0.5)
 
-                time.sleep(0.5)
-        else:
-            for x in range(1,int(input1)):
-                # print self.encoder.get_angle()
-                print 'Time=%f\t delta = %f\n' % (time.time() - start_time, 57.29577 * self.encoder.get_angle())
-                time.sleep(0.5)
-
-        input1 = raw_input('Press ENTER to start IMU test, move the bike body for the reading!')
+        # IMU
+        number_samples_IMU = raw_input('Input the number of samples of press ENTER for 20 samples for the IMU test, move the bike body for the reading! ')
         self.a_imu = IMU()
         start_time = time.time()
-        if not input1:
-            for x in range(1,20):
-                self.imudata = self.a_imu.get_imu_data()
-                print 'Time=%f\t'% (time.time() - start_time)
-                print(self.imudata)
-                # print(self.imudata)
-                time.sleep(0.5)
-        else:
-            for x in range(1,int(input1)):
-                # self.bike.get_imu_data()
-                self.imudata = self.a_imu.get_imu_data()
-                print 'Time=%g\t'% (time.time() - start_time)
-                # print(self.imudata)
-                print 'Temp=%g\tAx = %g\tAy = %g\tAz = %g\tGx = %g\tGy = %g\tGz = %g\t' % (
-                    self.imudata[0], self.imudata[1], self.imudata[2], self.imudata[3],
-                    self.imudata[4], self.imudata[5], self.imudata[6])
+        if not number_samples_IMU:
+            number_samples_IMU = 20
+        for x in range(1,int(number_samples_IMU)):
+            self.imudata = self.a_imu.get_imu_data()
+            print 'Time = %g\tTemp = %g\tAx = %g\tAy = %g\tAz = %g\tGx = %g\tGy = %g\tGz = %g\t' % (
+                time.time() - start_time), self.imudata[0], self.imudata[1], self.imudata[2], self.imudata[3],
+                self.imudata[4], self.imudata[5], self.imudata[6])
+             time.sleep(0.5)
 
-                time.sleep(0.5)
-
-
-        input2 = raw_input('Press ENTER to start GPS test, you can enter the #reading if you want')
+        # GPS
+        number_samples_GPS = raw_input('Input the number of samples of press ENTER for 20 samples for the GPS test, move the GPS for the reading! ')
         gps = GPS()
         start_time = time.time()
-        if not input2:
-            for x in range(1,10):
-                self.gpspos =  gps.get_position()
-                print 'Time=%f\t'% (time.time() - start_time)
-                print 'Temp=%g\tAx = %g\t' % (self.gpspos[0], self.gpspos[1])
-                # print(self.imudata)
-                time.sleep(1)
-        else:
-            for x in range(1,int(input2)):
-                self.gpspos = gps.get_position()
-                print 'Time=%f\t' % (time.time() - start_time)
-                print 'Temp=%g\tAx = %g\t' % (self.gpspos[0], self.gpspos[1])
-                # print(self.imudata)
-                time.sleep(1)
+        if not number_samples_GPS:
+            number_samples_GPS = 20
+        for x in range(1,int(number_samples_GPS)):
+            self.gpspos = gps.get_position()
+            print 'Time=%f\t' % (time.time() - start_time)
+            print 'Temp=%g\tAx = %g\t' % (self.gpspos[0], self.gpspos[1])
+            time.sleep(1)
 
+        # Drive motor
         input1 = raw_input('If all the tests are passed, then you may check the rear motor and steering motor. HOWEVER THEY ARE RISKY! PRESS ENTER TO CONTINUE')
         if not input1:
             # print 'Now we are going to test the rear motor, the gear is at LEVEL %i \t' %  RearMotorDrive().GearNr
-            input1 = raw_input('enter a velocity between 1-3.9, the test will last 30 secs')
+            input1 = raw_input('Input a velocity between 1 and 5 m/s, the test will last 30 secs ')
             self.rear_motor = RearMotorDrive()
             if not input1:
                 print 'Press enter to exit'
                 exit()
-            elif float(input1) >1 and  float(input1) < 10:
+            elif float(input1) >= 1 and  float(input1) <= 5:
                 input_velocity = float(input1)
                 raw_input('You need to be careful on the velocity reading, if they deviate too much, maybe there is problem in gear setting, see rearmotordrive.py!')
                 start_time = time.time()
