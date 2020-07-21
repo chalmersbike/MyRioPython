b38d3061e643ba7d892691495bfad64b8335424a
BBBlack1 for Black Bike <yixiao_wang@qq.com>
2020-07-21 13:19:49 +0200

    Regular update + 1 time step bouncetime on estop

--- sensors/safetystop.py
+++ sensors/safetystop.py
@@ -17,6 +17,8 @@ class SafetyStop(object):
             print "Safety stop : Chosen safety stop pull-up or pull-down type is not valid : %s. Choosing pull-up instead" %(safetyStop_pullUpDown)
             GPIO.setup(safetyStop_port, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+        self.ESTOP_previous = False
+
     def button_check(self):
         # If GPIO.input == 0 it means the stop button has been pressed
         if GPIO.input(safetyStop_port):
@@ -24,8 +26,14 @@ class SafetyStop(object):
         else:
             self.ESTOP = True
 
+        if not self.ESTOP == self.ESTOP_previous:
+            self.ESTOP = not self.ESTOP
+        self.ESTOP_previous = not bool(GPIO.input(safetyStop_port))
+
+        print("estop_gpio = %r ; estop = %r ; estop_previous = %r" % (bool(GPIO.input(safetyStop_port)),self.ESTOP,self.ESTOP_previous))
+
         # FOR TESTING PURPOSE ONLY, WILL IGNORE THE EMERGENCY STOP TO FORCEFULLY RUN THE CODE
         # The motors will still be electrically disconnected if the emergency stop is pressed
-        # self.ESTOP = False
+        self.ESTOP = False
 
         return self.ESTOP
